3e7a1d15f48c0df8d152e8640eba276114d422ce
Daniel Sabados <daniel@serviceseeking.com.au>
2012-07-26 19:41:28 +1000

    added host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,8 @@ Errbit::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set default host for ActionMailer links
-  config.action_mailer.default_url_options = { :host => ENV['ERRBIT_HOST'] } if ENV['ERRBIT_HOST']
+  #config.action_mailer.default_url_options = { :host => ENV['ERRBIT_HOST'] } if ENV['ERRBIT_HOST']
+  config.action_mailer.default_url_options = { :host => 'www.serviceseeking.com.au' }
 
   # Enable threaded mode
   # config.threadsafe!
